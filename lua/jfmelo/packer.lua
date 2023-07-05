8ca47e9e1b8d61153810a09f63d623d37b30cd07
dudemelo <dudemelo@gmail.com>
2023-07-05 13:56:23 +0200

    Remove unused keymaps

--- lua/jfmelo/packer.lua
+++ lua/jfmelo/packer.lua
@@ -4,16 +4,16 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use ({
-		'Mofiqul/dracula.nvim',
-		as = 'dracula',
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use({
+        'Mofiqul/dracula.nvim',
+        as = 'dracula',
         config = function()
             require('dracula').setup({
                 transparent_bg = true,
@@ -21,38 +21,37 @@ return require('packer').startup(function(use)
             vim.cmd('colorscheme dracula')
         end
     })
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-			ts_update()
-		end,
-	}
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('m4xshen/autoclose.nvim')
-	use('github/copilot.vim')
-	use('tpope/vim-fugitive')
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('m4xshen/autoclose.nvim')
+    use('github/copilot.vim')
+    use('tpope/vim-fugitive')
 
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                          -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
     }
-}
 end)
-
--- lua/jfmelo/set.lua
+++ lua/jfmelo/set.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 vim.opt.laststatus = 2
 vim.opt.cursorline = true
 vim.opt.nu = true
