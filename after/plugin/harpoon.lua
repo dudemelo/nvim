578047a3368c65cacc5613e7521088638367b0e1
dudemelo <dudemelo@gmail.com>
2023-07-07 18:18:09 +0200

    Simplify harpoon keymaps and remove unnecessary lualine configuration file

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,6 +3,8 @@ local ui = require('harpoon.ui')
 
 vim.keymap.set('n', '<leader>a', mark.add_file)
 vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
-
-vim.keymap.set('n', '<C-t>', function() ui.nav_next() end)
-vim.keymap.set('n', '<C-n>', function() ui.nav_prev() end)
+vim.keymap.set('n', 'h1', function() ui.nav_file(1) end)
+vim.keymap.set('n', 'h2', function() ui.nav_file(2) end)
+vim.keymap.set('n', 'h3', function() ui.nav_file(3) end)
+vim.keymap.set('n', 'h4', function() ui.nav_file(4) end)
+vim.keymap.set('n', 'h5', function() ui.nav_file(5) end)
--- lua/jfmelo/packer.lua
+++ lua/jfmelo/packer.lua
@@ -48,7 +48,15 @@ return require('packer').startup(function(use)
     }
     use {
         'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+        config = function()
+            require('lualine').setup({
+                options = {
+                    icons_enabled = false
+                },
+            })
+        end
+
     }
     use {
         'numToStr/Comment.nvim',
