848c47d1c9316ecf7d0b52717683277cd4ac7c55
dudemelo <dudemelo@gmail.com>
2023-07-08 20:50:25 +0200

    Highlight on yanking

--- lua/jfmelo/remap.lua
+++ lua/jfmelo/remap.lua
@@ -16,3 +16,13 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
