dcd167240abc2a03d92f260416681e7411966cb9
dudemelo <dudemelo@gmail.com>
2023-07-20 11:28:20 +0200

    Add emmet lsp configuration

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -43,6 +43,26 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end
 })
 
+-- emmet
+local lspconfig = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
+--
+
 lsp.setup()
 
 vim.diagnostic.config({ virtual_text = true })
